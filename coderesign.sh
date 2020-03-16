7188e6dd61b9af4cffcb5b501a15136d2f22cdee
kuperxu <kuperxu@tencent.com>
2020-03-17 00:08:48 +0800

    inject frida dylidb.

--- coderesign.sh
+++ coderesign.sh
@@ -16,7 +16,12 @@ TEMP_APP_PATH=$(set -- "$TEMP_PATH/Payload/"*.app;echo "$1")
 # echo "路径是:$TEMP_APP_PATH"
 
 #----------------------------------------
-# 2. 将解压出来的.app拷贝进入工程下
+# 2.Frida打包入APP，FRIDA_PATH修改为本地Frida路径
+FRIDA_PATH="/Users/xujiguang/Documents/reverse_engine/Frida/FridaGadget.dylib"
+cp "$FRIDA_PATH" "$TEMP_APP_PATH/FridaGadget.dylib"
+
+#----------------------------------------
+# 3. 将解压出来的.app拷贝进入工程下
 # BUILT_PRODUCTS_DIR 工程生成的APP包的路径
 # TARGET_NAME target名称
 # BUILT_PRODUCTS_DIR="${SRCROOT}/target"
@@ -28,21 +33,25 @@ mkdir -p "$TARGET_APP_PATH"
 cp -rf "$TEMP_APP_PATH/" "$TARGET_APP_PATH"
 
 #----------------------------------------
-# 3. 删除extension和WatchAPP.个人证书没法签名Extention
+# 4. 删除extension和WatchAPP.个人证书没法签名Extention
 rm -rf "$TARGET_APP_PATH/PlugIns"
 rm -rf "$TARGET_APP_PATH/Watch"
 
 #----------------------------------------
-# 4. 更新info.plist文件 CFBundleIdentifier
+# 5. 更新info.plist文件 CFBundleIdentifier
 #  设置:"Set : KEY Value" "目标文件路径"
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $PRODUCT_BUNDLE_IDENTIFIER" "$TARGET_APP_PATH/Info.plist"
 
 #----------------------------------------
-# 5. 给MachO文件上执行权限
+# 6. 给MachO文件上执行权限
 # 拿到MachO文件的路径
 APP_BINARY=`plutil -convert xml1 -o - $TARGET_APP_PATH/Info.plist|grep -A1 Exec|tail -n1|cut -f2 -d\>|cut -f1 -d\<`
 #上可执行权限
 chmod +x "$TARGET_APP_PATH/$APP_BINARY"
+
+#----------------------------------------
+# 7. 使用optool 注入Frida
+optool install -c load -p "$TARGET_APP_PATH/FridaGadget.dylib" -t "$TEMP_APP_PATH/Shadowrocket"
 
 #----------------------------------------
 # 6. 重签名第三方 FrameWorks
@@ -54,8 +63,9 @@ do
 
 # 7. s签名
 /usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$FRAMEWORK"
+/usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$TARGET_APP_PATH/FridaGadget.dylib"
 
 #清除数据
-rm -rf "${SRCROOT}/Temp"
+#rm -rf "${SRCROOT}/Temp"
 done
 fi
