6bb07289efe00d0361a3a6d22c819fb681787a7e
kuperxu <kuperxu@tencent.com>
2020-03-17 21:56:25 +0800

    --main project support frida

--- coderesign.sh
+++ coderesign.sh
@@ -4,6 +4,9 @@ TEMP_PATH="${SRCROOT}/Temp"
 ASSETS_PATH="${SRCROOT}/BreakJailAPP"
 #目标ipa包路径
 TARGET_IPA_PATH="${ASSETS_PATH}/*.ipa"
+
+FRIDA_PATH="/Users/kuperxu/Documents/reverse_engine/Frida/FridaGadget.dylib"
+
 #清空Temp文件夹
 rm -rf "${SRCROOT}/Temp"
 mkdir -p "${SRCROOT}/Temp"
@@ -13,12 +16,20 @@ mkdir -p "${SRCROOT}/Temp"
 unzip -oqq "$TARGET_IPA_PATH" -d "$TEMP_PATH"
 # 拿到解压的临时的APP的路径
 TEMP_APP_PATH=$(set -- "$TEMP_PATH/Payload/"*.app;echo "$1")
-# echo "路径是:$TEMP_APP_PATH"
+ echo "路径是:$TEMP_APP_PATH"
+ 
+# 获取被注入可执行文件的macho名字
+last_component=$(basename "$TEMP_APP_PATH")
+MACHO_NAME=${last_component%.*}
+echo $MACHO_NAME
 
 #----------------------------------------
-# 2.Frida打包入APP，FRIDA_PATH修改为本地Frida路径
-FRIDA_PATH="/Users/xujiguang/Documents/reverse_engine/Frida/FridaGadget.dylib"
-cp "$FRIDA_PATH" "$TEMP_APP_PATH/FridaGadget.dylib"
+# 2.Frida打包入APP，FRIDA_PATH修改为本地Frida路径 - Frida 需要自定义
+mkdir -p "$TEMP_APP_PATH/Frameworks"
+TARGET_FRIDA_PATH="$TEMP_APP_PATH/Frameworks/FridaGadget.dylib"
+cp "$FRIDA_PATH" "$TARGET_FRIDA_PATH"
+
+echo $TARGET_FRIDA_PATH
 
 #----------------------------------------
 # 3. 将解压出来的.app拷贝进入工程下
@@ -51,7 +62,7 @@ chmod +x "$TARGET_APP_PATH/$APP_BINARY"
 
 #----------------------------------------
 # 7. 使用optool 注入Frida
-optool install -c load -p "$TARGET_APP_PATH/FridaGadget.dylib" -t "$TEMP_APP_PATH/Shadowrocket"
+optool install -c load -p "$TARGET_FRIDA_PATH" -t "$TEMP_APP_PATH/$MACHO_NAME"
 
 #----------------------------------------
 # 6. 重签名第三方 FrameWorks
@@ -63,7 +74,7 @@ do
 
 # 7. s签名
 /usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$FRAMEWORK"
-/usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$TARGET_APP_PATH/FridaGadget.dylib"
+/usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$TARGET_FRIDA_PATH"
 
 #清除数据
 #rm -rf "${SRCROOT}/Temp"
