1e30175921c39e96aa4948af64da13d2456ced07
kuperxu <kuperxu@tencent.com>
2020-03-18 15:03:07 +0800

    --inject third part script success.

--- coderesign.sh
+++ coderesign.sh
@@ -7,35 +7,21 @@ TEMP_PATH="${SRCROOT}/Temp"
 ASSETS_PATH="${SRCROOT}/BreakJailAPP"
 #目标ipa包路径
 TARGET_IPA_PATH="${ASSETS_PATH}/*.ipa"
-
+#自定义 FridaGadget.dylib 注入库
 FRIDA_PATH="/Users/kuperxu/Documents/reverse_engine/Frida/FridaGadget.dylib"
-
-#清空Temp文件夹
-rm -rf "${SRCROOT}/Temp"
-mkdir -p "${SRCROOT}/Temp"
 
 #----------------------------------------
 # 1. 解压IPA到Temp下
+#清空Temp文件夹
+rm -rf "${SRCROOT}/Temp"
+mkdir -p "${SRCROOT}/Temp"
 unzip -oqq "$TARGET_IPA_PATH" -d "$TEMP_PATH"
 # 拿到解压的临时的APP的路径
 TEMP_APP_PATH=$(set -- "$TEMP_PATH/Payload/"*.app;echo "$1")
  echo "路径是:$TEMP_APP_PATH"
- 
-# 获取被注入可执行文件的macho名字
-last_component=$(basename "$TEMP_APP_PATH")
-MACHO_NAME=${last_component%.*}
-echo $MACHO_NAME
 
 #----------------------------------------
-# 2.Frida打包入APP，FRIDA_PATH修改为本地Frida路径 - Frida 需要自定义
-mkdir -p "$TEMP_APP_PATH/Frameworks"
-TARGET_FRIDA_PATH="$TEMP_APP_PATH/Frameworks/FridaGadget.dylib"
-cp "$FRIDA_PATH" "$TARGET_FRIDA_PATH"
-
-echo $TARGET_FRIDA_PATH
-
-#----------------------------------------
-# 3. 将解压出来的.app拷贝进入工程下
+# 2. 将解压出来的.app拷贝进入工程下
 # BUILT_PRODUCTS_DIR 工程生成的APP包的路径
 # TARGET_NAME target名称
 # BUILT_PRODUCTS_DIR="${SRCROOT}/target"
@@ -47,40 +33,45 @@ mkdir -p "$TARGET_APP_PATH"
 cp -rf "$TEMP_APP_PATH/" "$TARGET_APP_PATH"
 
 #----------------------------------------
-# 4. 删除extension和WatchAPP.个人证书没法签名Extention
+# 3. 个人证书没法签名Extention,删除extension和WatchAPP.
 rm -rf "$TARGET_APP_PATH/PlugIns"
 rm -rf "$TARGET_APP_PATH/Watch"
 
 #----------------------------------------
-# 5. 更新info.plist文件 CFBundleIdentifier
+# 4. 更新info.plist文件 CFBundleIdentifier
 #  设置:"Set : KEY Value" "目标文件路径"
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $PRODUCT_BUNDLE_IDENTIFIER" "$TARGET_APP_PATH/Info.plist"
 
 #----------------------------------------
-# 6. 给MachO文件上执行权限
-# 拿到MachO文件的路径
+# 5. 给MachO文件上执行权限
+# 获取被注入可执行文件的macho名字
 APP_BINARY=`plutil -convert xml1 -o - $TARGET_APP_PATH/Info.plist|grep -A1 Exec|tail -n1|cut -f2 -d\>|cut -f1 -d\<`
 #上可执行权限
 chmod +x "$TARGET_APP_PATH/$APP_BINARY"
 
 #----------------------------------------
-# 7. 使用optool 注入Frida
-optool install -c load -p "$TARGET_FRIDA_PATH" -t "$TEMP_APP_PATH/$MACHO_NAME"
+# 6. 使用optool 注入Frida
+# Frida打包入APP，FRIDA_PATH修改为本地Frida路径 - Frida 需要自定义
+# 这里要注意这里的签名不能和工程里面的 Embedded 相冲突。
+# 如果把FridaGadget设置成了embed&sign那么这里copy&sign就不需要做了。但是还是要有注入这一步。
+mkdir -p "$TARGET_APP_PATH/Frameworks"
+TARGET_FRIDA_PATH="$TARGET_APP_PATH/Frameworks/FridaGadget.dylib"
+cp "$FRIDA_PATH" "$TARGET_FRIDA_PATH"
+optool install -c load -p "@executable_path/Frameworks/FridaGadget.dylib" -t "$TARGET_APP_PATH/$APP_BINARY"
+echo $executable_path
 
 #----------------------------------------
-# 6. 重签名第三方 FrameWorks
+# 7. 重签名第三方 FrameWorks
 TARGET_APP_FRAMEWORKS_PATH="$TARGET_APP_PATH/Frameworks"
 if [ -d "$TARGET_APP_FRAMEWORKS_PATH" ];
 then
 for FRAMEWORK in "$TARGET_APP_FRAMEWORKS_PATH/"*
 do
-
-# 7. s签名
+echo $FRAMEWORK
 /usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$FRAMEWORK"
-/usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$TARGET_FRIDA_PATH"
 
-#清除数据
-#rm -rf "${SRCROOT}/Temp"
 done
 fi
 
+#清除数据
+rm -rf "${SRCROOT}/Temp"
